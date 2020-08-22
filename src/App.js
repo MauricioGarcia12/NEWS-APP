e260375fcb33f3c58bca5daa81e47885de1827bd
maumainsaw <maumainsaw@gmail.com>
2020-08-21 22:13:49 -0500

    Consultando API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React,{Fragment,useState} from 'react';
+import React,{Fragment,useState,useEffect} from 'react';
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 function App() {
   //definir categoria de noticias
-  const [categoria,guardarCategoria]=useState('')
+  const [categoria,guardarCategoria]=useState('');
+  const [noticias, guardarNoticias]=useState([]);
+
+  //CONSULTA AL API
+  useEffect(() => {
+    const consultarAPI = async () =>{
+      const url =`https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=efe5380d8dfa4a278641a5cdd6ef452a`;
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+      guardarNoticias(noticias.articles)
+     
+    }
+    consultarAPI()
+  }, [categoria])
+
+
   return (
     <Fragment>
       <Header
