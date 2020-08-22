3a233673bcf4d0a617e81700723d25d2363fa0bd
maumainsaw <maumainsaw@gmail.com>
2020-08-21 22:38:03 -0500

    Mostrar Noticias

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React,{Fragment,useState,useEffect} from 'react';
 import Header from './components/Header'
 import Formulario from './components/Formulario'
+import ListadoNoticias from './components/ListadoNoticias'
+
 function App() {
   //definir categoria de noticias
   const [categoria,guardarCategoria]=useState('');
@@ -27,8 +29,8 @@ function App() {
       <div className='container white'>
         <Formulario
         guardarCategoria={guardarCategoria}
-        
         />
+        <ListadoNoticias noticias={noticias}/>
       </div>
     </Fragment>
   );
